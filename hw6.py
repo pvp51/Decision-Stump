e5673444d9656bae11a7b9eeef6cb5bc07e7ef0e
Parth <parth5494@live.com>
2018-07-22 21:44:42 -0400

    updates

--- hw6.py
+++ hw6.py
@@ -11,23 +11,23 @@ import math
 ##best Split
 ###############
 
-def bestsplit(data, labels, col):
+def getBestSplit(data, labels, col):
     colvals = {}
     indices = []
-    rows = 0
+    dataRows = 0
     minus = 0
     for i in range(0, len(data), 1):
         if(labels.get(i) != None):
             colvals[i] = data[i][col]
             indices.append(i)
-            rows += 1
+            dataRows += 1
             if(labels[i] == 0):
                 minus += 1
 
     sorted_indices = sorted(indices, key=colvals.__getitem__)
 
     lsize = 1
-    rsize = rows - 1
+    rsize = dataRows - 1
     lp = 0
     rp = minus
     if(labels[sorted_indices[0]] == 0):
@@ -35,12 +35,12 @@ def bestsplit(data, labels, col):
         rp -= 1
 
     best_s = -1
-    bestgini = 10000
+    best_g = 10000
     for i in range(1, len(sorted_indices), 1):
         s = (colvals[sorted_indices[i]] + colvals[sorted_indices[i-1]])/2
-        gini = (lsize/rows)*(lp/lsize)*(1 - lp/lsize) + (rsize/rows)*(rp/rsize)*(1 - rp/rsize);
-        if(gini < bestgini):
-            bestgini = gini
+        gini = (lsize/dataRows)*(lp/lsize)*(1 - lp/lsize) + (rsize/dataRows)*(rp/rsize)*(1 - rp/rsize);
+        if(gini < best_g):
+            best_g = gini
             best_s = s
         if(labels[sorted_indices[i]] == 0):
             lp += 1
@@ -48,7 +48,7 @@ def bestsplit(data, labels, col):
         lsize += 1
         rsize -= 1
 
-    return(best_s, bestgini)
+    return(best_s, best_g)
 
 
 ################
@@ -64,8 +64,8 @@ while (l != ''):
         l2.append(float(a[j]))
     data.append(l2)
     l = f.readline()
-rows = len(data)
-cols = len(data[0])
+dataRows = len(data)
+dataCols = len(data[0])
 f.close()
 
 ################
@@ -87,8 +87,8 @@ f.close()
 best_split = -1
 best_col =1
 best_gini = 100000
-for j in range(0, cols, 1):
-    [s,gini] = bestsplit(data, trainlabels, j)
+for j in range(0, dataCols, 1):
+    [s,gini] = getBestSplit(data, trainlabels, j)
     print(s, gini)
     if(gini < best_gini):
         best_gini = gini
@@ -97,7 +97,7 @@ for j in range(0, cols, 1):
 
 m=0
 p=0
-for i in range(0, rows, 1):
+for i in range(0, dataRows, 1):
     if(trainlabels.get(i) != None):
         if(data[i][best_col] < best_split):
             if(trainlabels[i] == 0):
@@ -116,7 +116,7 @@ print(best_gini, best_split, best_col)
 ################
 ##Classify unlabeled points
 ###############
-for i in range(0, rows, 1):
+for i in range(0, dataRows, 1):
     if(trainlabels.get(i) == None):
         if(data[i][best_col] < best_split):
             print(left, i)
